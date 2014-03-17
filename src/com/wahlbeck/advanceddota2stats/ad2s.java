fe431ad9446dde0ca236046d5cd194562ea76589
David Wahlstrom <david.wahlstrom@gmail.com>
2014-03-16 19:56:35 -0700

    initial commit. nothing to see here (yet)

---
+++ src/com/wahlbeck/advanceddota2stats/ad2s.java
@@ -0,0 +1,9 @@
+public class ad2s {
+
+	public static void main(String[] args) {
+		scraper d2scraper = new scraper();
+		d2scraper.authenticate();
+		d2scraper.scrape();
+	}
+
+}
---
+++ src/com/wahlbeck/advanceddota2stats/scraper.java
@@ -0,0 +1,11 @@
+public class scraper {
+
+	public void authenticate() {
+		System.out.println("I AUTHED!");
+	}
+
+	public void scrape() {
+		System.out.println("I SCRAPED!");
+	}
+
+}
