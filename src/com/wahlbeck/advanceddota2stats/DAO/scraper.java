76c51823a8054ec2292fbdc9e2df3b43e6dc4fdb
David Wahlstrom <david.wahlstrom@gmail.com>
2014-03-17 20:28:44 -0700

    provide a slightly better idea of what whe are trying to accomplish

--- src/com/wahlbeck/advanceddota2stats/DAO/scraper.java
+++ src/com/wahlbeck/advanceddota2stats/DAO/scraper.java
@@ -1,9 +1,8 @@
 public class scraper {
 
 	public void getMatchHistory(String apikey) {
-		/*HttpClient client = new DefaultHttpClient();
-		HttpGet request = new HttpGet("https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v001/?key=" + apikey + "&account_id=0:42440556");*/
-		System.out.println(apikey);
+		String urlString = "https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v001/?key=" + apikey + "&account_id=0:42440556";
+		System.out.println(is);
 	}
 
 }
