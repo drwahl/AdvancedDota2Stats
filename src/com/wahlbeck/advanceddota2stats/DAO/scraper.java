21515f2a8d29f8131de26c0198a8394e8da6a093
David Wahlstrom <david.wahlstrom@gmail.com>
2014-03-17 20:38:04 -0700

    http get. ignoring exceptions at the moment. we will get back to that...

--- src/com/wahlbeck/advanceddota2stats/DAO/scraper.java
+++ src/com/wahlbeck/advanceddota2stats/DAO/scraper.java
@@ -1,8 +1,17 @@
+import java.net.*;
+import java.io.*;
+
 public class scraper {
 
-	public void getMatchHistory(String apikey) {
-		String urlString = "https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v001/?key=" + apikey + "&account_id=0:42440556";
-		System.out.println(is);
+	public void getMatchHistory(String apikey) throws Exception {
+		URL urlString = new URL("https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v001/?key=" + apikey + "&account_id=0:42440556");
+		BufferedReader in = new BufferedReader(
+		new InputStreamReader(urlString.openStream()));
+		String inputLine;
+		while ((inputLine = in.readLine()) != null)
+				System.out.println(inputLine);
+		in.close();
+		System.out.println(apikey);
 	}
 
 }
